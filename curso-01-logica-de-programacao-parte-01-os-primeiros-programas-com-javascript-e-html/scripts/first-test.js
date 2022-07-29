b0a6b33722b059b403dc13b8f0e19f242a0e0138
Thiago Bernegossi <thiago.bernegossi@outlook.com.br>
2022-07-28 23:33:23 -0300

    Curso 01 · Aula 01 · Atividade 06 · Dê olá ao mundo

---
+++ curso-01-logica-de-programacao-parte-01-os-primeiros-programas-com-javascript-e-html/scripts/first-test.js
@@ -0,0 +1,3 @@
+// Este código-fonte segue a definição completa dos padrões de codificação do Google para a linguagem de programação JavaScript.
+
+window.alert('Este sim é um programa informático!');
