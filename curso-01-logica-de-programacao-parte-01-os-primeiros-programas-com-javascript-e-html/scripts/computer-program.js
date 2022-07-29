d01670488f14e9920833692d9f56e636dd28bf23
Thiago Bernegossi <thiago.bernegossi@outlook.com.br>
2022-07-29 00:10:19 -0300

    Curso 01 · Aula 02 · Atividade 04 · Concatenação

--- curso-01-logica-de-programacao-parte-01-os-primeiros-programas-com-javascript-e-html/scripts/computer-program.js
+++ curso-01-logica-de-programacao-parte-01-os-primeiros-programas-com-javascript-e-html/scripts/computer-program.js
@@ -1,3 +1,3 @@
 // Este código-fonte segue a definição completa dos padrões de codificação do Google para a linguagem de programação JavaScript.
 
-window.alert('A idade de Donatello é 38 anos.');
+document.write(`A idade de Donatello é ${20 + 15} anos.`);
