2791ea8c90000a41bec07805bf35d28167da14d7
Thiago Bernegossi <thiago.bernegossi@outlook.com.br>
2022-07-29 00:34:44 -0300

    Curso 01 · Aula 02 · Atividade 06 · Operações com textos e números

--- curso-01-logica-de-programacao-parte-01-os-primeiros-programas-com-javascript-e-html/scripts/computer-program.js
+++ curso-01-logica-de-programacao-parte-01-os-primeiros-programas-com-javascript-e-html/scripts/computer-program.js
@@ -1,3 +1,6 @@
 // Este código-fonte segue a definição completa dos padrões de codificação do Google para a linguagem de programação JavaScript.
 
-document.write(`A idade de Donatello é ${20 + 15} anos.`);
+document.write(`Donatello nasceu no ano de ${2022 - 36}. <br>`);
+document.write(`Leonardo nasceu no ano de ${2022 - 35}. <br>`);
+document.write(`Michelangelo nasceu no ano de ${2022 - 34}. <br>`);
+document.write(`Raphael nasceu no ano de ${2022 - 33}.`);
