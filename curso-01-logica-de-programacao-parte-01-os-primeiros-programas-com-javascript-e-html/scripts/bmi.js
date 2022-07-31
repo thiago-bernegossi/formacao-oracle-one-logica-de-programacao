f403237de1b837ad268e2dc6ffb3106235cf9bfa
Thiago Bernegossi <thiago.bernegossi@outlook.com.br>
2022-07-31 15:56:11 -0300

    Curso 01 · Aula 06 · Atividade 07 · Melhorando programa de IMC

--- curso-01-logica-de-programacao-parte-01-os-primeiros-programas-com-javascript-e-html/scripts/bmi.js
+++ curso-01-logica-de-programacao-parte-01-os-primeiros-programas-com-javascript-e-html/scripts/bmi.js
@@ -16,5 +16,20 @@ function calculateBmi(userHeight, userWeight) {
 let userName = window.prompt(`01-) Olá! Para iniciarmos, digite o seu nome:`); 
 let userHeight = window.prompt(`02-) ${userName}, agora digite a sua altura:`); 
 let userWeight = window.prompt(`03-) ${userName}, agora digite o seu peso:`);
+let totalBmi = Math.round(calculateBmi(userHeight, userWeight));
 
-displayText(`${userName}, o valor do seu Índice de Massa Corporal é ${Math.round(calculateBmi(userHeight, userWeight))}.`);
+if (totalBmi < 18.5) {
+  displayText(`${userName}, o valor do seu Índice de Massa Corporal é ${totalBmi} e a categoria é subpeso.`);
+}
+
+if (totalBmi >= 18.5 && totalBmi <= 24.9) {
+  displayText(`${userName}, o valor do seu Índice de Massa Corporal é ${totalBmi} e a categoria é peso normal.`);
+}
+
+if (totalBmi >= 25 && totalBmi <= 29.9) {
+  displayText(`${userName}, o valor do seu Índice de Massa Corporal é ${totalBmi} e a categoria é sobrepeso.`);
+}
+
+if (totalBmi >= 30) {
+  displayText(`${userName}, o valor do seu Índice de Massa Corporal é ${totalBmi} e a categoria é obesidade.`);
+}
