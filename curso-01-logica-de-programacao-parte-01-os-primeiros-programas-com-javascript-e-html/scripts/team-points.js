89bcba7fa63e680d4b8f2b5d366da81440ffe484
Thiago Bernegossi <thiago.bernegossi@outlook.com.br>
2022-07-31 15:14:13 -0300

    Curso 01 · Aula 06 · Atividade 05 · Trabalhando com condições

--- curso-01-logica-de-programacao-parte-01-os-primeiros-programas-com-javascript-e-html/scripts/team-points.js
+++ curso-01-logica-de-programacao-parte-01-os-primeiros-programas-com-javascript-e-html/scripts/team-points.js
@@ -11,6 +11,17 @@ function displayText(parameter) {
 
 let userName = window.prompt(`01-) Olá! Para iniciarmos, digite o seu nome:`); 
 let totalWins = parseInt(window.prompt(`02-) ${userName}, agora digite o total de vitórias do time:`)); 
-let totalDraws = parseInt(window.prompt(`03-) ${userName}, agora digite o total de empates do time:`)); 
+let totalDraws = parseInt(window.prompt(`03-) ${userName}, agora digite o total de empates do time:`));
+let totalPoints = (totalWins * 3) + totalDraws;
 
-displayText(`${userName}, o total de pontos do time é ${(totalWins * 3) + totalDraws}.`);
+if (totalPoints > 33) {
+  displayText(`${userName}, o total de pontos do time neste ano é ${totalPoints}, sendo, portanto, maior que o total de pontos do ano passado.`);
+}
+
+if (totalPoints == 33) {
+  displayText(`${userName}, o total de pontos do time neste ano é ${totalPoints}, sendo, portanto, igual ao total de pontos do ano passado.`);
+}
+
+if (totalPoints < 33) {
+  displayText(`${userName}, o total de pontos do time neste ano é ${totalPoints}, sendo, portanto, menor que o total de pontos do ano passado.`);
+}
