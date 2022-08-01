55686ab0119c59c3294bcb2ae2e23e9e1a994930
Thiago Bernegossi <thiago.bernegossi@outlook.com.br>
2022-08-01 17:42:52 -0300

    Curso 01 · Aula 08 · Atividade 01 · Campo de texto e botão

---
+++ curso-01-logica-de-programacao-parte-01-os-primeiros-programas-com-javascript-e-html/scripts/new-random-number.js
@@ -0,0 +1,15 @@
+// Este código-fonte segue a definição completa dos padrões de codificação do Google para a linguagem de programação JavaScript.
+
+let randomNumber = Math.round(Math.random() * 10);
+let userNumber = document.querySelector('input');
+
+function process() {
+  if(userNumber.value == randomNumber) {
+    window.alert(`03-) Parabéns, você acertou! O número sorteado foi o ${randomNumber}.`);
+  } else {
+    window.alert(`03-) Infelizmente, você errou! O número sorteado não foi o ${userNumber.value}.`);
+  }
+}
+
+let sendButton = document.querySelector('button');
+sendButton.onclick = process;
