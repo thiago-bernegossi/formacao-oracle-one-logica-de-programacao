cd249ea3cc69cab8b16fc68733eae972dce8d93b
Thiago Bernegossi <thiago.bernegossi@outlook.com.br>
2022-08-01 22:03:12 -0300

    Curso 01 · Aula 09 · Atividade 01 · Armazenando muitos dados

--- curso-01-logica-de-programacao-parte-01-os-primeiros-programas-com-javascript-e-html/scripts/new-random-number.js
+++ curso-01-logica-de-programacao-parte-01-os-primeiros-programas-com-javascript-e-html/scripts/new-random-number.js
@@ -1,12 +1,20 @@
 // Este código-fonte segue a definição completa dos padrões de codificação do Google para a linguagem de programação JavaScript.
 
-let randomNumber = Math.round(Math.random() * 10);
+let numbers = [1, 5, 10];
 let userNumber = document.querySelector('input');
 
 function process() {
-  if(userNumber.value == randomNumber) {
-    window.alert(`03-) Parabéns, você acertou! O número sorteado foi o ${randomNumber}.`);
-  } else {
+  let foundTheNumbers = false;
+
+  for (let position = 0; position < numbers.length; position++) {
+    if(userNumber.value == numbers[position]) {
+      window.alert(`03-) Parabéns, você acertou! O número sorteado foi o ${numbers[position]}.`);
+      foundTheNumbers = true;
+      break;
+    } 
+  }
+
+  if(foundTheNumbers == false) {
     window.alert(`03-) Infelizmente, você errou! O número sorteado não foi o ${userNumber.value}.`);
   }
 
