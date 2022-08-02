698e4381265faa11de52574cb2ce22c5372d0933
Thiago Bernegossi <thiago.bernegossi@outlook.com.br>
2022-08-02 16:41:56 -0300

    Curso 02 · Aula 01 · Atividade 03 · Desenhando com um pincel

--- curso-02-logica-de-programacao-parte-02-pratique-com-desenhos-animacoes-e-um-jogo/scripts/computer-program.js
+++ curso-02-logica-de-programacao-parte-02-pratique-com-desenhos-animacoes-e-um-jogo/scripts/computer-program.js
@@ -1,1 +1,7 @@
 // Este código-fonte segue a definição completa dos padrões de codificação do Google para a linguagem de programação JavaScript.
+
+let screen = document.querySelector('canvas');
+let brush = screen.getContext('2d');
+
+brush.fillStyle = '#d3d3d3';
+brush.fillRect(0, 0, 600, 400);
