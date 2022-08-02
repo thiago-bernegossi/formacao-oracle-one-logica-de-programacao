e1cc33009b201e5931babe2302a2bc387b911225
Thiago Bernegossi <thiago.bernegossi@outlook.com.br>
2022-08-02 17:06:40 -0300

    Curso 02 · Aula 01 · Atividade 05 · A vida não é só feita de retângulos!

--- curso-02-logica-de-programacao-parte-02-pratique-com-desenhos-animacoes-e-um-jogo/scripts/computer-program.js
+++ curso-02-logica-de-programacao-parte-02-pratique-com-desenhos-animacoes-e-um-jogo/scripts/computer-program.js
@@ -3,11 +3,25 @@
 let screen = document.querySelector('canvas');
 let brush = screen.getContext('2d');
 
-brush.fillStyle = '#d3d3d3';
+brush.fillStyle = '#e6e6fa';
 brush.fillRect(0, 0, 600, 400);
 
-brush.fillStyle = '#000080';
+brush.fillStyle = '#0a75ad';
 brush.fillRect(0, 0, 200, 400);
 
-brush.fillStyle = '#cc0000';
+brush.fillStyle = '#2acaea';
 brush.fillRect(400, 0, 400, 400);
+
+brush.fillStyle = '#ccff00';
+brush.beginPath();
+brush.moveTo(200, 0);
+brush.lineTo(300, 200);
+brush.lineTo(400, 0);
+brush.fill();
+
+brush.fillStyle = '#ccff00';
+brush.beginPath();
+brush.moveTo(300, 200);
+brush.lineTo(200, 400);
+brush.lineTo(400, 400);
+brush.fill();
