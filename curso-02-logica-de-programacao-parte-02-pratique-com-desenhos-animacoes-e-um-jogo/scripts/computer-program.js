85dbd06a237e5ad4f4a63d7c208b5e8279d38e08
Thiago Bernegossi <thiago.bernegossi@outlook.com.br>
2022-08-02 16:51:59 -0300

    Curso 02 · Aula 01 · Atividade 04 · Nossa primeira obra de arte. Será?

--- curso-02-logica-de-programacao-parte-02-pratique-com-desenhos-animacoes-e-um-jogo/scripts/computer-program.js
+++ curso-02-logica-de-programacao-parte-02-pratique-com-desenhos-animacoes-e-um-jogo/scripts/computer-program.js
@@ -5,3 +5,9 @@ let brush = screen.getContext('2d');
 
 brush.fillStyle = '#d3d3d3';
 brush.fillRect(0, 0, 600, 400);
+
+brush.fillStyle = '#000080';
+brush.fillRect(0, 0, 200, 400);
+
+brush.fillStyle = '#cc0000';
+brush.fillRect(400, 0, 400, 400);
