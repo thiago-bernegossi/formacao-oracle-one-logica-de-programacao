1e47259fb9d9fb6e7e4c1ccc976b5d7390cec2d9
Thiago Bernegossi <thiago.bernegossi@outlook.com.br>
2022-08-04 17:10:24 -0300

    Curso 02 · Aula 05 · Atividade 02 · O alvo aleatório!

--- curso-02-logica-de-programacao-parte-02-pratique-com-desenhos-animacoes-e-um-jogo/scripts/random-target.js
+++ curso-02-logica-de-programacao-parte-02-pratique-com-desenhos-animacoes-e-um-jogo/scripts/random-target.js
@@ -3,16 +3,38 @@
 let screen = document.querySelector('canvas');
 let brush = screen.getContext('2d');
 
-brush.fillStyle = '#665f68';
+brush.fillStyle = '#fff';
 brush.fillRect(0, 0, 600, 400);
 
-function drawCircle(xAxis, yAxis, radius) {
-  brush.fillStyle = '#1c42ba';
+function drawCircle(xAxis, yAxis, radius, color) {
+  brush.fillStyle = color;
   brush.beginPath();
   brush.arc(xAxis, yAxis, radius, 0, 2 * Math.PI);
   brush.fill();
 }
 
+let radius = 10;
+
+function drawTarget(xAxis, yAxis) {
+  drawCircle(xAxis, yAxis, radius + 20, '#dc143c');
+  drawCircle(xAxis, yAxis, radius + 10, '#fff');
+  drawCircle(xAxis, yAxis, radius, '#dc143c');
+}
+
+function moveTarget(maximumValue) {
+  return Math.floor(Math.random() * maximumValue);
+}
+
 function cleanScreen() {
   brush.clearRect(0, 0, 600, 400);
 }
+
+function refreshScreen() {
+  cleanScreen();
+
+  let xAxis = moveTarget(600);
+  let yAxis = moveTarget(400);  
+  drawTarget(xAxis, yAxis);
+}
+
+setInterval(refreshScreen, 1000);
