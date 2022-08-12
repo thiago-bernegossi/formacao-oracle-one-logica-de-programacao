217e2fb47e3b763473d870fb1e0bb035bed734fd
Thiago Bernegossi <thiago.bernegossi@outlook.com.br>
2022-08-11 21:55:27 -0300

    Curso 05 · Aula 01 · Atividade 07 · Query Selector

--- curso-05-javascript-programando-na-linguagem-da-web/scripts/main.js
+++ curso-05-javascript-programando-na-linguagem-da-web/scripts/main.js
@@ -1,1 +1,4 @@
 // Este código-fonte segue a definição completa dos padrões de codificação do Google para a linguagem de programação JavaScript.
+
+let companyName = (document.querySelector('h2'));
+companyName.textContent = 'Lista de Pacientes';
