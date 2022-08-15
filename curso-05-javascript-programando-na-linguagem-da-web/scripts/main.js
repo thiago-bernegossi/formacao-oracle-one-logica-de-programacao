7a8ff82ed25fd5e1f44147a06591677c4bb6f54d
Thiago Bernegossi <thiago.bernegossi@outlook.com.br>
2022-08-15 16:55:08 -0300

    Curso 05 · Aula 03 · Atividade 01 · Replicando a validação e o cálculo do IMC para todos os pacientes

--- curso-05-javascript-programando-na-linguagem-da-web/scripts/main.js
+++ curso-05-javascript-programando-na-linguagem-da-web/scripts/main.js
@@ -3,30 +3,34 @@
 let companyName = (document.querySelector('.secondary-title'));
 companyName.textContent = 'Lista de Pacientes';
 
-let patient = document.querySelector('#first-patient');
+let patients = document.querySelectorAll('.patient');
 
-let infoWeight = patient.querySelector('.info-weight');
-let weight = infoWeight.textContent;
+for (let counter = 0; counter < patients.length; counter++) {
+  let patient = patients[counter]; 
 
-let infoHeight = patient.querySelector('.info-height');
-let height = infoHeight.textContent;
-
-let infoBmi = patient.querySelector('.info-bmi');
-
-let weightIsValid = true;
-let heightIsValid = true;
-
-if (weight <= 0 || weight >= 1000) {
-  weightIsValid = false;
-  infoBmi.textContent = `O valor do peso é inválido!`;
-}
-
-if (height <= 0 || height >= 3.00) {
-  heightIsValid = false;
-  infoBmi.textContent = `O valor da altura é inválido!`;
-}
-
-if (weightIsValid === true && heightIsValid === true) {
-  let bmi = weight / (height * height);
-  infoBmi.textContent = Math.round(bmi);
+  let infoWeight = patient.querySelector('.info-weight');
+  let weight = infoWeight.textContent;
+  
+  let infoHeight = patient.querySelector('.info-height');
+  let height = infoHeight.textContent;
+  
+  let infoBmi = patient.querySelector('.info-bmi');
+  
+  let weightIsValid = true;
+  let heightIsValid = true;
+  
+  if (weight <= 0 || weight >= 1000) {
+    weightIsValid = false;
+    infoBmi.textContent = `O valor do peso é inválido!`;
+  }
+  
+  if (height <= 0 || height >= 3.00) {
+    heightIsValid = false;
+    infoBmi.textContent = `O valor da altura é inválido!`;
+  }
+  
+  if (weightIsValid === true && heightIsValid === true) {
+    let bmi = weight / (height * height);
+    infoBmi.textContent = bmi.toFixed(2);
+  }
 }
