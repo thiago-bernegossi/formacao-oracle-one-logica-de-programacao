a561d4cf7996ab79c0a9b56d10bcd61f8ce74f4f
Thiago Bernegossi <thiago.bernegossi@outlook.com.br>
2022-08-19 17:32:55 -0300

    Curso 05 · Aula 07 · Atividade 03 · Delegando eventos

--- curso-05-javascript-programando-na-linguagem-da-web/scripts/delete-patient.js
+++ curso-05-javascript-programando-na-linguagem-da-web/scripts/delete-patient.js
@@ -1,10 +1,9 @@
 // Este código-fonte segue a definição completa dos padrões de codificação do Google para a linguagem de programação JavaScript.
 
-let patientsToRemove = document.querySelectorAll('.patient');
-console.log(patientsToRemove);
+let patientsToRemove = document.querySelector('table');
 
-patientsToRemove.forEach(function(patient) {
-  patient.addEventListener('dblclick', function() {
-    this.remove();
-  });
+patientsToRemove.addEventListener('dblclick', function(event) {
+  let eventTarget = event.target;
+  let eventTargetParent = eventTarget.parentNode;
+  eventTargetParent.remove();
 });
